eda66af83e8266585dd3d4c9f16a96ec9af22c63
“Linda <linda.resseguier@gmail.com>
2021-01-04 12:37:55 +0100

    manual input validation done for missing field and invalid email

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+/* eslint-disable quotes */
 // dotenv loads parameters (port and database config) from .env
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const connection = require('./db');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./db");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // respond to requests on `/api/users`
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   // send an SQL query to get all users
-  connection.query('SELECT * FROM user', (err, results) => {
+  connection.query("SELECT * FROM user", (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).json({
@@ -25,9 +26,26 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({
+      error: "at least one of the required fields is missing",
+    });
+  }
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+  if (!emailRegex.test(email)) {
+    return res.status(422).json({
+      error: "Invalid email",
+    });
+  }
+  if (password.length < 8) {
+    return res.status(422).json({
+      error: "Password too short (8 characters min.)",
+    });
+  }
   // send an SQL query to get all users
-  connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
+  connection.query("INSERT INTO user SET ?", req.body, (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).json({
@@ -43,7 +61,7 @@ app.post('/api/users', (req, res) => {
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
-    throw new Error('Something bad happened...');
+    throw new Error("Something bad happened...");
   }
 
   console.log(`Server is listening on ${process.env.PORT}`);
